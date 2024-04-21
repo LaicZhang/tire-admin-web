f20c4d40c3871b01f5b1600e196289a820b2024d
laiczhang <i@laiczhang.com>
2024-04-21 22:41:23 +0800

    feat: update company api

--- src/api/company.ts
+++ src/api/company.ts
@@ -4,36 +4,30 @@ import type { CommonResult } from "./type";
 
 const prefix = "/company";
 
-export function getCompanyListApi(params?: Object) {
+export function getCompanyListApi(index: number, params?: Object) {
   return http.request<CommonResult>(
     "get",
-    baseUrlApi(prefix + "company/page"),
+    baseUrlApi(prefix + "/page/" + index),
     { params }
   );
 }
 
 export function addCompanyApi(data: Object) {
-  return http.request<CommonResult>("post", baseUrlApi(prefix + "company"), {
+  return http.request<CommonResult>("post", baseUrlApi(prefix), {
     data
   });
 }
 
 export function getCompanyApi(uid: string) {
-  return http.request<CommonResult>(
-    "get",
-    baseUrlApi(prefix + "company/" + uid)
-  );
+  return http.request<CommonResult>("get", baseUrlApi(prefix + "/" + uid));
 }
 
-export function updateCompanyApi(data: Object) {
-  return http.request<CommonResult>("patch", baseUrlApi(prefix + "company"), {
+export function updateCompanyApi(uid: string, data: Object) {
+  return http.request<CommonResult>("patch", baseUrlApi(prefix + "/" + uid), {
     data
   });
 }
 
 export function deleteCompanyApi(uid: number) {
-  return http.request<CommonResult>(
-    "delete",
-    baseUrlApi(prefix + "company/" + uid)
-  );
+  return http.request<CommonResult>("delete", baseUrlApi(prefix + "/" + uid));
 }
