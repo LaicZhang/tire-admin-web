64bbf39fec509cc3f97a3b9a57afd342256268ff
laiczhang <i@laiczhang.com>
2024-11-27 10:49:18 +0800

    chore: use crypto instead of crypto-js

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,14 +1,24 @@
-import CryptoJS from "crypto-js";
+// import CryptoJS from "crypto-js";
+import crypto from "crypto";
 import { formatTimeOnlyNumber } from "@/utils/time";
+
+const hash =
+  crypto.hash ??
+  ((
+    algorithm: string,
+    data: crypto.BinaryLike,
+    outputEncoding: crypto.BinaryToTextEncoding
+  ) => crypto.createHash(algorithm).update(data).digest(outputEncoding));
 
 export function getMD5(data: Buffer | string): string {
   // return CryptoJS.MD5(data).toString().toLowerCase();
-  return CryptoJS.MD5(CryptoJS.lib.WordArray.create(data)).toString();
+  // return CryptoJS.MD5(CryptoJS.lib.WordArray.create(data)).toString();
+  return hash("md5", data, "hex");
 }
 
 export function getFileMd5(lastModified: number, size: number): string {
   try {
-    return CryptoJS.MD5(`${lastModified}${size}`).toString().toLowerCase();
+    return getMD5(`${lastModified}${size}`).toLowerCase();
   } catch (e) {
     throw new Error(e);
   }
