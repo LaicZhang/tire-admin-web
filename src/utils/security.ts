f65d29350f55afbba513052b09aecc76955ea20a
laiczhang <i@laiczhang.com>
2025-01-08 16:05:37 +0800

    chore: update md5 function

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,39 +1,17 @@
 import { formatTimeOnlyNumber } from "@/utils/time";
+import CryptoJS from "crypto-js";
 
-export async function getMD5(message: string) {
-  // return CryptoJS.MD5(data).toString().toLowerCase();
+export function getMD5(data: string) {
+  return CryptoJS.MD5(data).toString().toLowerCase();
   // return CryptoJS.MD5(CryptoJS.lib.WordArray.create(data)).toString();
-  // return hash("md5", data, "hex");
-  try {
-    const encoder = new TextEncoder();
-    const messageBytes = encoder.encode(message);
-    const hashBuffer = await crypto.subtle.digest("MD5", messageBytes);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray
-      .map(b => b.toString(16).padStart(2, "0"))
-      .join("");
-    return hashHex;
-  } catch (error) {
-    console.error("MD5 hashing error:", error);
-    throw new Error(error);
-  }
 }
 
-export async function getFileMd5(lastModified: number, size: number) {
+export function getFileMd5(lastModified: number, size: number) {
   try {
-    const md5 = await getMD5(`${lastModified}${size}`);
-    return md5.toLowerCase();
+    return getMD5(`${lastModified}${size}`);
   } catch (e) {
     throw new Error(e);
   }
-}
-
-export async function compareMD5(hash: string, data: string) {
-  const fileHash = await getMD5(data);
-  if (hash !== fileHash)
-    throw new Error(
-      `the parma ${hash} and real hash ${fileHash} hash not match`
-    );
 }
 
 export function getUsernameOfOnlyNumber() {
