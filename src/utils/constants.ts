7ba67399d1b72b7f56a5e15f82648ffe0da4c190
laiczhang <i@laiczhang.com>
2026-01-21 16:00:42 +0800

    feat(utils): add constants file for shared constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+/**
+ * 分页相关常量
+ */
+
+/** 默认分页大小 */
+export const DEFAULT_PAGE_SIZE = 20;
+
+/** 批量获取时使用的分页大小（用于下拉列表等需要较多数据的场景） */
+export const BATCH_FETCH_PAGE_SIZE = 100;
+
+/** 获取全部数据时使用的最大分页大小 */
+export const MAX_FETCH_PAGE_SIZE = 1000;
